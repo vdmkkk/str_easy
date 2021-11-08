e2187627a9a06b9f5debbc7118968a2eeb8cddcf
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-11-08 12:36:28 +0300

    Update itc_equal_reverse.cpp

--- itc_equal_reverse.cpp
+++ itc_equal_reverse.cpp
@@ -1,6 +1,6 @@
 #include "str_easy.h"
 
 bool itc_equal_reverse(string str){
-    if (itc_reverse_string(str) == str) return true;
+    if (itc_reverse_str(str) == str) return true;
     return false;
 }
