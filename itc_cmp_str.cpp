bbd06e602c5a7a19b12adf0dae758dab5e5533ba
Vadim Soloviov <50838513+Vodemka13@users.noreply.github.com>
2021-11-11 12:38:08 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -1,5 +1,20 @@
 #include "str_easy.h"
 
 string itc_cmp_str(string str1, string str2, int num){
-    return itc_slice_str(str1, 0, num - 1) + str2 + itc_slice_str(str1, num, itc_len(str1) - itc_len(str2) - 1);
+
+    long long len1 = itc_len(str1);
+    long long len2 = itc_len(str2);
+    string newstring = "";
+
+    for (int i = 0; i <= num - 1; i++){
+        newstring += str1[i];
+    }
+
+    newstring += itc_slice_str(str2, 0, (len1 - num));
+
+    for (int j = num ; j < len1; j++){
+        newstring += str1[j];
+        }
+
+    return itc_slice_str(newstring, 0, len1 - 1);
 }
